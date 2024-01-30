a87528e6fdce2de0f254acac9aed0c212d099d76
SeifEleslam <seifgouda98@gmail.com>
2024-01-30 07:10:08 +0200

    first 10 tasks

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -5,7 +5,7 @@
  * @tree: tree to map
  * Return: true if leaf
  */
-size_t binary_tree_depth(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
