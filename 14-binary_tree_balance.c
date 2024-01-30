399aa4a46dce7806541bb65c7666aaa507ecbf34
SeifEleslam <seifgouda98@gmail.com>
2024-01-30 16:17:41 +0200

    task14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - check if leaf
+ * @tree: tree to map
+ * Return: true if leaf
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int addition = 0;
+
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	addition += tree->left ? 1 : 0;
+	addition -= tree->right ? 1 : 0;
+
+	return (addition +
+			binary_tree_balance(tree->left) - binary_tree_balance(tree->right));
+}
