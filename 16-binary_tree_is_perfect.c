d1fb2ae58b51f2ec51646d6ab7fe0acc90a5537f
SeifEleslam <seifgouda98@gmail.com>
2024-01-31 00:36:00 +0200

    task16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,49 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * power - check if leaf
- * @base: base
- * @exponent: exponent
- * Return: power
- */
-int power(int base, int exponent)
-{
-	int result = 1;
-
-	for (int i = 0; i < exponent; i++)
-		result *= base;
-	return (result);
-}
-
-/**
- * tree_height - check if leaf
+ * is_prefect - check if perfect
  * @tree: tree to map
  * Return: true if leaf
  */
-int tree_height(const binary_tree_t *tree)
+int is_prefect(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	int left, right;
 
-	if (!tree)
-		return (0);
-
-	left_height = tree_height(tree->left);
-	right_height = tree_height(tree->right);
-	return (1 + MAX(left_height, right_height));
-}
-
-/**
- * tree_leaves - check if leaf
- * @tree: tree to map
- * Return: true if leaf
- */
-int tree_leaves(const binary_tree_t *tree)
-{
 	if (!tree)
 		return (0);
 	if (!tree->left && !tree->right)
 		return (1);
-	return (tree_leaves(tree->left) + tree_leaves(tree->right));
+	left = is_prefect(tree->left);
+	right = is_prefect(tree->right);
+	return (left > 0 && right > 0 && left == right ? left + right : 0);
 }
 
 /**
@@ -51,10 +23,7 @@ int tree_leaves(const binary_tree_t *tree)
  * @tree: tree to map
  * Return: true if leaf
  */
-
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
-	return ((tree_leaves(tree) == power(2, tree_height(tree) - 1)) ? 1 : 0);
+	return (is_prefect(tree) > 0 ? 1 : 0);
 }
