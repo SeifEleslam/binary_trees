0b4ac44039eed8324e0c4e83ee8412c0c35d327b
SeifEleslam <seifgouda98@gmail.com>
2024-01-30 07:50:17 +0200

    first 10 tasks

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,12 +7,10 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
 	size_t left_height = binary_tree_height(tree->left);
 	size_t right_height = binary_tree_height(tree->right);
-	size_t add = tree->left || tree->right ? 1 : 0;
-
-	return (add + (left_height > right_height ? left_height : right_height));
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
