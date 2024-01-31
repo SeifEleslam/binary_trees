9b0a5cfa7fc04a17ef982ff2d7130a981a2bff84
SeifEleslam <seifgouda98@gmail.com>
2024-01-31 22:41:23 +0200

    task21

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,55 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_level - check if leaf
+ * @tree: tree to map
+ * @min: func to run
+ * @max: func to run
+ * Return: num of nodes
+ */
+int tree_is_complete(const binary_tree_t *tree, int level, int *min, int *max)
+{
+	int left, right;
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		if (*max == -1 || *min == -1)
+		{
+			*max = level, *min = level - 1;
+			return (1);
+		}
+		else if (level < *min || level > *max)
+			return (0);
+		else
+		{
+			*max = level;
+			return (1);
+		}
+	left = tree_is_complete(tree->left, level + 1, min, max);
+	right = tree_is_complete(tree->right, level + 1, min, max);
+	if (!tree->left)
+		return (0);
+	if (!tree->right)
+		if (level >= *min && level <= *max)
+		{
+			*max = level;
+			return ((!tree->left ? right : left));
+		}
+		else
+			return (0);
+	return (left && right ? 1 : 0);
+}
+
+/**
+ * binary_tree_is_complete - check if leaf
+ * @tree: tree to map
+ * Return: int
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	int min_level = -1, max_level = -1;
+
+	if (!tree)
+		return (0);
+	return tree_is_complete(tree, 0, &min_level, &max_level);
+}
