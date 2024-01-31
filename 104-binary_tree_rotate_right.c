d8d70cd0f82e0025e711ec2a3cc18b33c1717834
SeifEleslam <seifgouda98@gmail.com>
2024-01-31 23:49:42 +0200

    task23

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_right - check if leaf
+ * @tree: tree to map
+ * Return: int
+ */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *node;
+
+	if (!tree || !tree->left)
+		return (tree);
+	node = tree->left;
+	tree->left = node->right;
+	node->right = tree;
+	node->parent = tree->parent;
+	tree->parent = node;
+	return (node);
+}
