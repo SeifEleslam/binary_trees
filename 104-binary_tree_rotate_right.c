4f366e81b83a69066a5f3eb2aadf5075e4d4c07d
SeifEleslam <seifgouda98@gmail.com>
2024-02-01 00:32:47 +0200

    task18

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -11,11 +11,18 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 
 	if (!tree || !tree->left)
 		return (tree);
-
 	node = tree->left;
 	tree->left = node->right;
 	node->right = tree;
 	node->parent = tree->parent;
 	tree->parent = node;
+	if (tree->left)
+		tree->left->parent = tree;
+	if (node->parent)
+		if (node->parent->left == tree)
+			node->parent->left = node;
+		else
+			node->parent->right = node;
+
 	return (node);
 }
