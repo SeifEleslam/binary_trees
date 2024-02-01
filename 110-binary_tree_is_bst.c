276e31feca2c4a38e4fc3973c87418dc923344ae
SeifEleslam <seifgouda98@gmail.com>
2024-02-01 13:18:25 +0200

    task110

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -5,6 +5,22 @@
  * @tree: tree to map
  * Return: int
  */
+int node_is_bst(const binary_tree_t *tree, int val, int is_left)
+{
+	if (!tree)
+		return (1);
+	return (node_is_bst(tree->left, val, is_left) &&
+					node_is_bst(tree->right, val, is_left) &&
+					((is_left && tree->n < val) || !is_left && tree->n > val)
+				? 1
+				: 0);
+}
+
+/**
+ * binary_tree_is_bst - check if leaf
+ * @tree: tree to map
+ * Return: int
+ */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	int is_bst = 1, left, right;
@@ -14,24 +30,8 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 
 	left = !tree->left || binary_tree_is_bst(tree->left) ? 1 : 0;
 	right = !tree->right || binary_tree_is_bst(tree->right) ? 1 : 0;
-	if (tree->left)
-		is_bst = is_bst && tree->n > tree->left->n ? 1 : 0;
-	if (tree->right)
-		is_bst = is_bst && tree->n < tree->right->n ? 1 : 0;
-	if (tree->parent)
-	{
-		if (tree->parent->left == tree)
-			is_bst = is_bst &&
-							 (!tree->left || tree->parent->n > tree->left->n) &&
-							 (!tree->right || tree->parent->n > tree->right->n)
-						 ? 1
-						 : 0;
-		else
-			is_bst = is_bst &&
-							 (!tree->left || tree->parent->n < tree->left->n) &&
-							 (!tree->right || tree->parent->n < tree->right->n)
-						 ? 1
-						 : 0;
-	}
-	return (left && right && is_bst ? 1 : 0);
+	return (node_is_bst(tree->left, tree->n, 1) &&
+					node_is_bst(tree->right, tree->n, 0) && left && right
+				? 1
+				: 0);
 }
